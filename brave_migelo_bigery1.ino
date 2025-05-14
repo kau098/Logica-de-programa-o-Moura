57865314a60715d6455670cbb2c074502b230892
kau098 <kauasilveiravalerio01@gmail.com>
2025-05-14 09:37:28 -0300

    Add files via upload

---
+++ brave_migelo_bigery1.ino
@@ -0,0 +1,27 @@
+// calcule a média entre duas notas
+//exiba se a média maior ou giual a 6 e se a frequencia é maior que 75%
+//caso contrário, exiba se for reprovado por nota
+//exiba se for reprovado por frequencia
+//exiba se for reprovado por frequencia ou por nota
+//se a média for exatamente 10 e a frequencia for 100 exiba: "Parabens! Nota máxima!".
+
+int nota  = 6;
+
+
+void setup ()
+{
+Serial.begin(9600);
+    
+ Serial.println("Verificar se o aluno foi aprovado ou reprovado")
+   
+ Serial.println();
+  
+  Serial.println (nota <= 6 e frequencia <= 74)//reprovado por frequencia
+ {  Serial.println("reprovado")
+   else if (nota <= 5 e frequencia 75)//reprovado por nota
+}
+   
+   
+   
+   
+   
